453d08fdb41b6ddb9af97565403cb4ff9b6e098b
JacksonOtto <100025144@mvla.net>
2022-05-17 11:34:43 -0700

    minor format change

--- src/pages/Attendance/Attendance.tsx
+++ src/pages/Attendance/Attendance.tsx
@@ -1,46 +1,54 @@
-import { useEffect, useState } from "react";
-import { addPoints, getLeaderboard } from "../../firebase";
-import { PointsType, PointHistory } from "../../interfaces";
+import { useEffect, useState } from 'react';
+import { addPoints, getLeaderboard } from '../../firebase';
+import { PointsType, PointHistory } from '../../interfaces';
 import './Attendance.scss';
 
 export default function Attendance() {
-    const [members, setMembers] = useState<PointsType[]>([]);
+  const [members, setMembers] = useState<PointsType[]>([]);
 
-	useEffect(() => {	
-		(async function handleGetLeaderboard(): Promise<void> {
-			try {
-				const leaderboardData = await getLeaderboard();
-				setMembers(leaderboardData);
-			} catch (e: any) {}
-		})();
-		
-	}, [])
-    const checkAlreadyMarked = (member: any): boolean => {
-	let alreadyMarked: boolean = false;
-	member.history.forEach((val: PointHistory) => {
-		if (val.reason === "attending meeting" && val.date === new Date().toLocaleDateString()) {
-			alreadyMarked = true;
-			return;
-		}
-	})
-	return alreadyMarked;
-    }
-    const markAsPresent = async (member: any) => {
-	
-	if (!checkAlreadyMarked(member))
-        	await addPoints(member.uid, member.name, "attending meeting", 50);
-    }
-    return (
-        <div className="attendance">
-            <h1> Attendance for {new Date().toLocaleDateString()}</h1>
-            <ul>
-                {members.map((member, i) => (
-                    <li key = {i}>
-                        {member.name}
-				    {checkAlreadyMarked(member)? " Already marked present" :  <button onClick = {() => markAsPresent(member)}> Mark as Present </button>}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  useEffect(() => {
+    (async function handleGetLeaderboard(): Promise<void> {
+      try {
+        const leaderboardData = await getLeaderboard();
+        setMembers(leaderboardData);
+      } catch (e: any) {}
+    })();
+  }, []);
+  const checkAlreadyMarked = (member: any): boolean => {
+    let alreadyMarked: boolean = false;
+    member.history.forEach((val: PointHistory) => {
+      if (
+        val.reason === 'attending meeting' &&
+        val.date === new Date().toLocaleDateString()
+      ) {
+        alreadyMarked = true;
+        return;
+      }
+    });
+    return alreadyMarked;
+  };
+  const markAsPresent = async (member: any) => {
+    if (!checkAlreadyMarked(member))
+      await addPoints(member.uid, member.name, 'attending meeting', 50);
+  };
+  return (
+    <div className="attendance">
+      <h1> Attendance for {new Date().toLocaleDateString()}</h1>
+      <ul>
+        {members.map((member, i) => (
+          <li key={i}>
+            {member.name}
+            {checkAlreadyMarked(member) ? (
+              ' Already marked present'
+            ) : (
+              <button onClick={() => markAsPresent(member)}>
+                {' '}
+                Mark as Present{' '}
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
