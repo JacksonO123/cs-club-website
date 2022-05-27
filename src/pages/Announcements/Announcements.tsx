f9a5c087b436518a9b74d16510ae9454a4eb9d3c
JacksonOtto <100025144@mvla.net>
2022-05-27 11:33:22 -0700

    sort announcements

--- src/pages/Announcements/Announcements.tsx
+++ src/pages/Announcements/Announcements.tsx
@@ -44,7 +44,7 @@ export default function Annoucements({ user, isAdmin }: Props) {
   }, []);
 
   return (
-    <div className='announcements'>
+    <div className="announcements">
       {addingAnnouncement ? (
         <NewAnnouncement
           user={user}
@@ -57,21 +57,23 @@ export default function Annoucements({ user, isAdmin }: Props) {
       {announcements.length > 0 ? (
         <>
           <h1>Announcements</h1>
-          <div className='announcement-list'>
-            {announcements.map((announcement: any, index: number): any => (
-              <Announcement
-                key={`${index}-announcement-id`}
-                announcement={announcement}
-                isAdmin={isAdmin}
-              />
-            ))}
+          <div className="announcement-list">
+            {announcements
+              .sort((a: any, b: any) => b.timestamp - a.timestamp)
+              .map((announcement: any, index: number): any => (
+                <Announcement
+                  key={`${index}-announcement-id`}
+                  announcement={announcement}
+                  isAdmin={isAdmin}
+                />
+              ))}
           </div>
         </>
       ) : (
         <h1>No announcements yet</h1>
       )}
       {isAdmin ? (
-        <Fab sx={fabStyles} color='primary' onClick={handleAddingAnnouncement}>
+        <Fab sx={fabStyles} color="primary" onClick={handleAddingAnnouncement}>
           <AddIcon />
         </Fab>
       ) : (
